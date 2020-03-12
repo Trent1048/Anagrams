3cbfffa9ef55d02473cd0d9d5b3662e9171f3216
Trent1048 <trentbultsma@gmail.com>
2020-03-12 10:38:40 -0700

    set up all required methods

---
+++ src/Anagrams.java
@@ -0,0 +1,21 @@
+import java.util.Set;
+
+public class Anagrams {
+
+    public Anagrams(Set<String> dictionary) {
+
+    }
+
+    public Set<String> getWords(String phrase) {
+        return null;
+    }
+
+    public void print(String phrase) {
+
+    }
+
+    public void print(String phrase, int max) {
+
+    }
+
+}
