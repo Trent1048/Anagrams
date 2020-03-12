b62418aed84c3c0091ff1fcd6abb6797d5fb10b3
Trent1048 <trentbultsma@gmail.com>
2020-03-12 10:49:58 -0700

    add cases for exceptions

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -3,19 +3,27 @@ import java.util.Set;
 public class Anagrams {
 
     public Anagrams(Set<String> dictionary) {
-
+        if(dictionary == null) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Set<String> getWords(String phrase) {
+        if(phrase == null) {
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 
     public void print(String phrase) {
-
+        print(phrase, 0);
     }
 
     public void print(String phrase, int max) {
-
+        if(phrase == null || max < 0) {
+            throw new IllegalArgumentException();
+        } else if(phrase.equals("")) {
+            return;
+        }
     }
-
 }
