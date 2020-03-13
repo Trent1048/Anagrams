a46521941ef8015bde48d14410493d718bdab407
Trent1048 <trentbultsma@gmail.com>
2020-03-12 18:13:59 -0700

    add functions to calculate the anagrams and print them out

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -7,8 +7,9 @@ public class Anagrams {
     private Set<String> dictionary;
     // store results from previous inputted phrase
     // used to not recalculate the words if a phrase is inputted twice
-    private Set<String> wordsInPhrase;
     private String phrase;
+    private Set<String> wordsInPhrase;
+    private Set<Set<String>> anagrams;
 
     // sets up the dictionary set
     public Anagrams(Set<String> dictionary) {
@@ -24,13 +25,13 @@ public class Anagrams {
             throw new IllegalArgumentException();
         }
         // if the word has already been calculated, don't recalculate it
-        if(phrase.equals(this.phrase)) {
-            return wordsInPhrase;
-        } else {
+        if(!phrase.equals(this.phrase)) {
+            // calculate the words and anagrams in the phrase
             this.phrase = phrase;
             wordsInPhrase = calculateWords(phrase);
-            return wordsInPhrase;
+            getAnagrams();
         }
+        return wordsInPhrase;
     }
 
     // actually calculates the words found in the phrase
@@ -45,6 +46,41 @@ public class Anagrams {
         return wordsFromPhrase;
     }
 
+    // finds all the anagrams of phrase within wordsInPhrase and adds them to anagrams
+    private void getAnagrams() {
+        anagrams = new HashSet<>();
+        LetterInventory phraseLI = new LetterInventory(phrase);
+        for(String word : wordsInPhrase) {
+            LetterInventory remainingLetters = new LetterInventory(phrase);
+            remainingLetters.subtract(word);
+            calculateAnagrams(word, remainingLetters, new HashSet<>());
+        }
+    }
+
+    // recursively calculates the anagrams within phrase and adds them to the anagrams set
+    private void calculateAnagrams(String word, LetterInventory remainingLetters, Set<String> anagramsFound) {
+        anagramsFound.add(word);
+        if(remainingLetters.isEmpty()) {
+            // when there are no more letters, add it to anagrams and move on
+            anagrams.add(anagramsFound);
+        } else {
+            for(String newWord : wordsInPhrase) {
+                LetterInventory newWordLI = new LetterInventory(newWord);
+                // must create a new LetterInventory with the same letters in it as remainingLetters
+                // so remainingLetters isn't passed as a reference and doesn't mess up the whole thing
+                // by having it's letters removed before they are used in different parts on the
+                // entire recursive stack
+                String remainingLettersAsString = remainingLetters.toString();
+                LetterInventory newRemainingLetters = new LetterInventory(
+                        remainingLettersAsString.substring(1, remainingLettersAsString.length()-1));
+                if(remainingLetters.contains(newWordLI)) {
+                    newRemainingLetters.subtract(newWordLI);
+                    calculateAnagrams(newWord, newRemainingLetters, new HashSet<>(anagramsFound));
+                }
+            }
+        }
+    }
+
     // prints out all anagrams of a phrase
     public void print(String phrase) {
         print(phrase, 0);
@@ -56,6 +92,13 @@ public class Anagrams {
             throw new IllegalArgumentException();
         } else if(phrase.equals("")) {
             return;
+        }
+        // updates anagrams by calling getWords()
+        getWords(phrase);
+        for(Set<String> anagram : anagrams) {
+            if(max == 0 || anagram.size() <= max) {
+                System.out.println(anagram);
+            }
         }
     }
 }
